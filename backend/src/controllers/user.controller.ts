670db9597ee804c6729afe8b6581bc38e7032a6c
Hamid Raza <hamidramay2002@gmail.com>
2025-12-15 14:24:26 +0500

    feat: include user object in authentication response

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -130,7 +130,7 @@ export const checkAuthStatus = asyncHandler(async (req: Request, res: Response) 
     return res
         .status(200)
         .json(
-            new ApiResponse(200, req.user, 'User is authenticated')
+            new ApiResponse(200, {user: req.user}, 'User is authenticated')
         )
 })
 
