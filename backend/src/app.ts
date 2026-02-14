deffe6d84af874cceebce0d393187c094c4d2a6e
Hamid Raza <hamidramay2002@gmail.com>
2026-02-14 10:21:21 +0500

    Refactor app.ts for improved structure and static file serving

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,9 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import dotenv from "dotenv"
+import path from "path"
+import { fileURLToPath } from "url"
+
 import { errorHandler } from "./middlewares/errorHandler.middleware.js"
 
 // import routes
@@ -9,7 +12,7 @@ import userRouter from "./routes/user.route.js"
 import outwardGatePassRouter from "./routes/outwardGatePass.route.js"
 
 dotenv.config({
-    path: './.env'
+    path: "./.env"
 })
 
 export const app = express()
@@ -20,17 +23,32 @@ app.use(cors({
 }))
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
-app.get("/", (_req, res) => {
+
+app.get("/health", (_req, res) => {
     res.status(200).json({
         status: true
     })
 })
 
+
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/outward-gate-pass", outwardGatePassRouter)
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const frontendPath = path.join(__dirname, "../../frontend/dist")
+
+// Serve static files
+app.use(express.static(frontendPath))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"))
+})
+
 
 app.use(errorHandler)
