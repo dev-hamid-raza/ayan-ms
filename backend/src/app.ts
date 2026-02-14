e21736597727800ecb08ce2b3b9b43eb62dfd263
Hamid Raza <hamidramay2002@gmail.com>
2026-02-14 10:25:25 +0500

    Fix route handling for serving the main HTML file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -46,7 +46,7 @@ const frontendPath = path.join(__dirname, "../../frontend/dist")
 // Serve static files
 app.use(express.static(frontendPath))
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"))
 })
 
