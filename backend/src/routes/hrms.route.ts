ba14cdb854cbae53520b4c3cc7cc490253979538
Hamid Raza <hamidramay2002@gmail.com>
2026-03-02 15:02:47 +0500

    refactor: Simplify route definitions by removing redundant verifyJWT middleware

--- backend/src/routes/hrms.route.ts
+++ backend/src/routes/hrms.route.ts
@@ -34,37 +34,41 @@ import {
 
 const router = Router()
 
+router.use(verifyJWT)
+
 // ========== attendance routes ===========
-router.route('/attendance/punch').post(verifyJWT, punchTime)
-router.route('/attendance/get-punches').get(verifyJWT, getPunches)
+router.route('/attendance/punch').post(punchTime)
+router.route('/attendance/get-punches').get(getPunches)
 
 // ========== department routes ===========
-router.route('/department/create').post(verifyJWT, createDepartment)
-router.route('/department/update/:id').post(verifyJWT, updateDepartment)
-router.route('/department/delete/:id').delete(verifyJWT, deleteDepartment)
-router.route('/department/').get(verifyJWT, departmentList)
+router.route('/departments')
+    .get(departmentList)
+    .post(createDepartment)
+router.route('/departments/:id')
+    .post(updateDepartment)
+    .delete(deleteDepartment)
 
 // ========== designation routes ===========
-router.route('/designation').get(verifyJWT, designationList)
-router.route('/designation/create').post(verifyJWT, createDesignation)
-router.route('/designation/delete/:id').delete(verifyJWT, deleteDesignation)
-router.route('/designation/update/:id').post(verifyJWT, updateDesignation)
+router.route('/designation').get(designationList)
+router.route('/designation/create').post(createDesignation)
+router.route('/designation/delete/:id').delete(deleteDesignation)
+router.route('/designation/update/:id').post(updateDesignation)
 
 // ========== employee routes ===========
-router.route('/employee').get(verifyJWT, employeesList)
-router.route('/employee/create').post(verifyJWT, upload.single('image'), createEmployee)
-router.route('/employee/update/:id').post(verifyJWT, upload.single('image'), updateEmployee)
+router.route('/employee').get(employeesList)
+router.route('/employee/create').post(upload.single('image'), createEmployee)
+router.route('/employee/update/:id').post(upload.single('image'), updateEmployee)
 
 // ========== employeeType routes ===========
-router.route('/emp-type').get(verifyJWT, empTypeList)
-router.route('/emp-type/create').post(verifyJWT, createEmpType)
-router.route('/emp-type/delete/:id').delete(verifyJWT, deleteEmpType)
-router.route('/emp-type/update/:id').post(verifyJWT, updateEmpType)
+router.route('/emp-type').get(empTypeList)
+router.route('/emp-type/create').post(createEmpType)
+router.route('/emp-type/delete/:id').delete(deleteEmpType)
+router.route('/emp-type/update/:id').post(updateEmpType)
 
 // ========== shifts routes ===========
-router.route('shift').get(verifyJWT, shiftList)
-router.route('shift/create').post(verifyJWT, shiftCreate)
-router.route('shift/delete/:id').delete(verifyJWT, deleteShift)
-router.route('shift/update/:id').post(verifyJWT, updateShift)
+router.route('shift').get(shiftList)
+router.route('shift/create').post(shiftCreate)
+router.route('shift/delete/:id').delete(deleteShift)
+router.route('shift/update/:id').post(updateShift)
 
 export default router
